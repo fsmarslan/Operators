a5a2f5413b8c4e6f01d100a9d346fbee12cd3cb1
Fatih Arslan <fatih1122335@gmail.com>
2022-08-23 15:50:47 +0300

    operatorler islendi

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Atama ve işlemli atama
+            int x = 3;
+            int y = 3;
+            y = y+2;
+            Console.WriteLine(y);
+            y +=2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            // mantıksal operatörler
+            // ||, &&, !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if(isSuccess && isCompleted)
+            {
+                Console.WriteLine("perfect");
+            }
+
+            if(isSuccess || isCompleted)
+            {
+                Console.WriteLine("Great");
+            }
+
+            if(isSuccess && !isCompleted)
+            {
+                Console.WriteLine("Fine");
+            }
+
+            // ilişkisel operatörler
+            // <, >, >=, <=, ==, !=
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a<b;
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc = a<=b;
+            Console.WriteLine(sonuc);
+            sonuc = a>=b;
+            Console.WriteLine(sonuc);
+            sonuc = a==b;
+            Console.WriteLine(sonuc);
+            sonuc = a!=b;
+            Console.WriteLine(sonuc);
+
+            //aritmatiksel operatörler
+            // +  -  *  /
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1-sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1*sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1/sayi2;
+            Console.WriteLine(sonuc1);
+
+            // % : mod alır
+            int sonuc2 = 20%3;
+            Console.WriteLine(sonuc2);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
